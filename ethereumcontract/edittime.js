346f97caf9c7bf84447015e3d546ce29e99f4f3e
Jeremy Quentin <jeremy.quentin@gmail.com>
2017-12-30 16:42:31 +0100

    Add Ethereum Contract plugin

---
+++ ethereumcontract/edittime.js
@@ -0,0 +1,148 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Ethereum Contract",
+		"id":			"EthereumContract",
+		"version":		"1.0",
+		"description":	"Access Ethereum API features for Contracts.",
+		"author":		"Scirra",
+		"help url":		"http://www.scirra.com/manual/112/Ethereum",
+		"category":		"Platform specific",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0,
+		"cordova-plugins": "cordova-plugin-inappbrowser",
+		"dependency":	"channel.html"
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+AddCondition(0, cf_trigger, "On contract loaded", "Ethereum", "On contract loaded", "Triggered when the contract is loaded", "OnContractLoaded");
+
+AddStringParam("ID", "The id of the callback to wait for.");
+AddCondition(1, cf_trigger, "On function success", "Callbacks", "On function success with ID: {0}", "Triggered when a function returns with no error.", "OnFunctionSuccess");
+
+AddStringParam("ID", "The id of the callback to wait for.");
+AddCondition(2, cf_trigger, "On function error", "Callbacks", "On function error with ID: {0}", "Triggered when a function returns with an error.", "OnFunctionError");
+
+AddStringParam("ID", "The id of the callback to wait for.");
+AddCondition(3, cf_trigger, "On function callback", "Callbacks", "On function callback with ID: {0}", "Triggered when a function returns, with or without error.", "OnFunctionCallback");
+
+AddStringParam("Event", "The name of the event to watch for.");
+AddCondition(4, cf_trigger, "On event", "Event", "On event: {0}", "Triggered when an event is triggered.", "OnEvent");
+
+//////////////////////////////////////////////////////////////
+// Actions
+
+AddStringParam("Name with parameters types", "The name of the function to call, in the form 'myMethod(uint256)'.");
+AddVariadicParams("Parameter {n}", "A parameter to pass for the function call.");
+AddStringParam("Callback id", "The id for identifying the callback.");
+AddAction(0, 0, "Call", "Methods", "Call constant method {0} ( {...} )", "Call a Constant Method", "Call");
+
+AddStringParam("Name with parameters types", "The name of the function to call, in the form 'myMethod(uint256)'.");
+AddVariadicParams("Parameter {n}", "A parameter to pass for the function call.");
+AddStringParam("Callback id", "The id for identifying the callback.");
+AddNumberParam("Value", "The value sent to the transaction");
+AddAction(1, 0, "Send", "Methods", "Call non-constant method: {0} ( {...} )", "Call a Non-Constant Method", "Send");
+
+AddStringParam("Name with parameters types", "The name of the function to call, in the form 'myMethod(uint256)'.");
+AddVariadicParams("Parameter {n}", "A parameter to pass for the function call.");
+AddStringParam("Callback id", "The id for identifying the callback.");
+AddAction(2, 0, "Estimate Gas", "Methods", "Estimate gas of {0} ( {...} )", "Estimate gas of a Non-Constant Method", "EstimateGas");
+
+AddStringParam("Hash", "The transaction hash.");
+AddStringParam("Callback id", "The id for identifying the callback.");
+AddAction(3, 0, "Get Transaction Receipt", "Blocks", "Returns the receipt of transaction {0}", "Returns the receipt of a transaction by transaction hash", "GetTransactionReceipt");
+
+
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+AddExpression(0, ef_return_string, "Current Account", "Ethereum", "CurrentAccount", "Get the Current Account Address.");
+AddExpression(1, ef_return_string, "Current Callback Function", "Ethereum", "CurrentCallbackFunction", "Get the Current Callback Function.");
+AddExpression(2, ef_return_string, "Current Callback ID", "Ethereum", "CurrentCallbackId", "Get the Current Callback ID.");
+AddExpression(3, ef_return_string, "Current Callback Error Message", "Ethereum", "CurrentCallbackError", "Get the Current Callback Function's error message.");
+AddExpression(4, ef_return_string, "Current Callback Response", "Ethereum", "CurrentCallbackResponse", "Get the Current Callback Function's response.");
+
+AddNumberParam("Number", "The value in Unit");
+AddStringParam("Unit", "The unit to convert from.");
+AddExpression(5, ef_return_string, "Converts into wei", "Ethereum", "ToWei", "Converts an ethereum unit into wei.");
+
+AddNumberParam("Number", "The value in Wei");
+AddStringParam("Unit", "The unit to convert to.");
+AddExpression(6, ef_return_string, "Converts from wei", "Ethereum", "FromWei", "Converts a number of wei into ethereum units.");
+
+//AddVariadicParams("Elements", "The elements to hash.");
+AddExpression(7, ef_return_string | ef_variadic_parameters, "Hash the elements", "Ethereum", "Sha3", "Hashes the elements using web3.sha3.");
+
+AddStringParam("hex String", "An HEX string.");
+AddExpression(8, ef_return_number, "Checks if the given string is an address", "Ethereum", "IsAddress", "Checks if the given string is an address.");
+
+
+//////////////////////////////////////////////////////////////
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_text,		"Contract address",		"",			"The contract's address."),
+	new cr.Property(ept_text,		"Contract ABI",			"",			"The contract's ABI array")
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ ethereumcontract/runtime.js
@@ -0,0 +1,255 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.EthereumContract = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.EthereumContract.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+	var runtime;
+	var inst;
+	
+	
+	var contractInstance;
+	
+	var currentCallbackFunction;
+	var currentCallbackId;
+	var currentCallbackResponse;
+	var currentCallbackError;
+	var currentEvent;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+		if (this.runtime.isDomFree)
+		{
+			cr.logexport("[Construct 2] EthereumContract plugin not supported on this platform - the object will not be created");
+			return;
+		}
+		
+		this.runtime.tickMe(this);
+		
+		runtime = this.runtime;
+		inst = this;
+		
+		var contractAddress = this.properties[0];
+		var contractABI = JSON.parse(this.properties[1]);
+		
+		var MyContract = web3.eth.contract(contractABI);
+		contractInstance = MyContract.at(contractAddress);
+		
+		var events = contractInstance.allEvents();
+
+		// watch for changes
+		events.watch(function(err, ev){
+			if (!err)
+			{
+				currentEvent = ev;
+				runtime.trigger(pluginProto.cnds.OnEvent, inst);
+			}
+		});
+		
+		runtime.trigger(pluginProto.cnds.OnContractLoaded, inst);
+
+	};
+	
+	instanceProto.tick = function ()
+	{
+	};
+	
+	instanceProto.onLayoutChange = function ()
+	{
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	
+	Cnds.prototype.OnContractLoaded = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.OnFunctionSuccess = function (id)
+	{
+		return currentCallbackId == id;
+	};
+	Cnds.prototype.OnFunctionError = function (id)
+	{
+		return currentCallbackId == id;
+	};
+	Cnds.prototype.OnFunctionCallback = function (id)
+	{
+		return currentCallbackId == id;
+	};
+	Cnds.prototype.OnEvent = function (ev)
+	{
+		return currentEvent == ev;
+	};
+	
+	pluginProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	
+	Acts.prototype.Call = function (name, paramsArray, id)
+	{
+		if (paramsArray.length == 0 || paramsArray.length == 1 && paramsArray[0] == '')
+			contractInstance[name].call(function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 1)
+			contractInstance[name].call(paramsArray[0], function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 2)
+			contractInstance[name].call(paramsArray[0], paramsArray[1], function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 3)
+			contractInstance[name].call(paramsArray[0], paramsArray[1], paramsArray[2], function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 4)
+			contractInstance[name].call(paramsArray[0], paramsArray[1], paramsArray[2], paramsArray[3], function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 5)
+			contractInstance[name].call(paramsArray[0], paramsArray[1], paramsArray[2], paramsArray[3], paramsArray[4], function (err, res) { OnSendCallback (err, res, id); });
+	}
+	
+	Acts.prototype.Send = function (name, paramsArray, id, v)
+	{
+		if (paramsArray.length == 0 || paramsArray.length == 1 && paramsArray[0] == '')
+			contractInstance[name].sendTransaction({value:v}, function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 1)
+			contractInstance[name].sendTransaction(paramsArray[0], {value:v}, function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 2)
+			contractInstance[name].sendTransaction(paramsArray[0], paramsArray[1], {value:v}, function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 3)
+			contractInstance[name].sendTransaction(paramsArray[0], paramsArray[1], paramsArray[2], {value:v}, function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 4)
+			contractInstance[name].sendTransaction(paramsArray[0], paramsArray[1], paramsArray[2], paramsArray[3], {value:v}, function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 5)
+			contractInstance[name].sendTransaction(paramsArray[0], paramsArray[1], paramsArray[2], paramsArray[3], paramsArray[4], {value:v}, function (err, res) { OnSendCallback (err, res, id); });
+	}
+	
+	Acts.prototype.EstimateGas = function (name, paramsArray, id)
+	{
+		if (paramsArray.length == 0 || paramsArray.length == 1 && paramsArray[0] == '')
+			contractInstance[name].estimateGas(function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 1)
+			contractInstance[name].estimateGas(paramsArray[0], function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 2)
+			contractInstance[name].estimateGas(paramsArray[0], paramsArray[1], function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 3)
+			contractInstance[name].estimateGas(paramsArray[0], paramsArray[1], paramsArray[2], function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 4)
+			contractInstance[name].estimateGas(paramsArray[0], paramsArray[1], paramsArray[2], paramsArray[3], function (err, res) { OnSendCallback (err, res, id); });
+		else if (paramsArray.length == 5)
+			contractInstance[name].estimateGas(paramsArray[0], paramsArray[1], paramsArray[2], paramsArray[3], paramsArray[4], function (err, res) { OnSendCallback (err, res, id); });
+	}
+	
+	
+	Acts.prototype.GetTransactionReceipt = function (hash, id)
+	{
+		web3.eth.getTransactionReceipt(hash, function (err, res) { OnSendCallback (err, res, id); });
+	}
+	
+	function OnSendCallback (error, result, id)
+	{
+		currentCallbackId = id;
+		currentCallbackFunction = name;
+		if (error)
+		{
+			currentCallbackResponse = "";
+			currentCallbackError = error;
+			runtime.trigger(cr.plugins_.EthereumContract.prototype.cnds.OnFunctionError, inst);
+			runtime.trigger(cr.plugins_.EthereumContract.prototype.cnds.OnFunctionCallback, inst);
+		}
+		else
+		{
+			currentCallbackError = "";
+			currentCallbackResponse = result;
+			runtime.trigger(cr.plugins_.EthereumContract.prototype.cnds.OnFunctionSuccess, inst);
+			runtime.trigger(cr.plugins_.EthereumContract.prototype.cnds.OnFunctionCallback, inst);
+		}
+	}
+	
+	pluginProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	
+	
+	Exps.prototype.CurrentAccount = function (ret)
+	{
+		ret.set_string(web3.eth.coinbase.toString());
+	};
+	Exps.prototype.CurrentCallbackFunction = function (ret)
+	{
+		ret.set_string(currentCallbackFunction);
+	};
+	Exps.prototype.CurrentCallbackId = function (ret)
+	{
+		ret.set_string(currentCallbackId);
+	};
+	Exps.prototype.CurrentCallbackError = function (ret)
+	{
+		ret.set_string(currentCallbackError ? currentCallbackError.toString() : "");
+	};
+	Exps.prototype.CurrentCallbackResponse = function (ret)
+	{
+		ret.set_string(currentCallbackResponse ? currentCallbackResponse.toString() : "");
+	};
+	// Returns string because BigNumbers are not supported by set_int
+	Exps.prototype.ToWei = function (ret, number, unit)
+	{
+		ret.set_string(web3.toWei(number, unit).toString());
+	};
+	// Returns string because BigNumbers are not supported by set_int
+	Exps.prototype.FromWei = function (ret, number, unit)
+	{
+		ret.set_string(web3.fromWei(number, unit).toString());
+	};
+	Exps.prototype.Sha3 = function (ret, arg1_, arg2_, arg3_, arg4_, arg5_)
+	{
+		var arg1 = arg1_ || "";
+		var arg2 = arg2_ || "";
+		var arg3 = arg3_ || "";
+		var arg4 = arg4_ || "";
+		var arg5 = arg5_ || "";
+		
+		ret.set_string(web3.sha3(arg1 + arg2 + arg3 + arg4 + arg5));
+	};
+	Exps.prototype.IsAddress = function (ret, hexString)
+	{
+		ret.set_int(web3.isAddress(hexString) ? 1 : 0);
+	};
+	
+	
+	pluginProto.exps = new Exps();
+
+}());
